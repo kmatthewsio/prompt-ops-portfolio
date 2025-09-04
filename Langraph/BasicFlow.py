5a3af3037660aaec9d396c4aa5db5594531f53a9
2krypto2quit <2Krypto2Kwit@pm.me>
2025-09-04 07:36:54 -0400

    lang graph basics

--- Langraph/BasicFlow.py
+++ Langraph/BasicFlow.py
@@ -7,7 +7,34 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = init_chat_model("anthropic:claude-3-5-sonnet-latest")
+llm = init_chat_model("openai:gpt-4")  # or gpt-3.5-turbo, gpt-4o, etc.
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+graph_builder = StateGraph(State)
+
+def chatbot(state: State):
+    return {
+        "messages": [llm.invoke(state["messages"])]
+    }
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+
+graph = graph_builder.compile()
+
+user_input = input("Enter a message: ")
+state = graph.invoke({
+    "messages": [
+        {
+            "role": "user",
+            "content": user_input
+        }
+    ]
+})
+
+print(state["messages"][-1].content)
 
 
-print('Hello world')
+
